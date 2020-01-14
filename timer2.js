cd760e4824daac098e155c7814119daef343996c
Carolina Contreras Issa <agirlnamedcaro@gmail.com>
2020-01-14 23:40:28 +0000

    Interactive timer app

---
+++ timer2.js
@@ -0,0 +1,17 @@
+const stdin = process.stdin;
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
+stdin.on('data', (key) => {
+    if ((key >= 1) || (key <= 9)) {
+        process.stdout.write(`Setting timer for ${key} seconds...\n`)
+        setTimeout(() => {
+            process.stdout.write('\x07')
+        }, key * 1000);
+    }
+    if (key === "b") {
+        process.stdout.write('\x07');
+    }
+    if (key === '\u0003') {
+        process.exit();
+    }
+})
